5239218a7813b039ae80eceaa0fb1cc75da8c645
Kseniia Ivanchenko <k.ivanchenko-2027@kpi.ua>
2023-04-23 21:49:22 +0200

    final version

--- floyd_algo.py
+++ floyd_algo.py
@@ -1,6 +1,5 @@
-import multiprocessing as mp
-from threading import Thread
-import concurrent.futures
+from multiprocessing import Array as SynchronizedArray
+from concurrent.futures import ProcessPoolExecutor
 
 def floyd_sequential(graph):
     n = graph.size
@@ -11,62 +10,31 @@ def floyd_sequential(graph):
                     graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j])
     return graph
 
-
 def floyd_parallel(graph):
-    n = graph.size
-    INF = float('inf')
-    dist = [[INF]*n for _ in range(n)]
-    for i in range(n):
-        dist[i][i] = 0
-    
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = []
-        for k in range(n):
-            for i in range(n):
-                futures.append(executor.submit(update_distance, graph, dist, i, k))
-        
-        for future in concurrent.futures.as_completed(futures):
-            pass
-        
-    return dist
-
-def update_distance(graph, dist, i, k):
-    n = graph.size
-    for j in range(n):
-        if dist[i][k] + graph[k][j] < dist[i][j]:
-            dist[i][j] = dist[i][k] + graph[k][j]
-    
-    return dist[i][j]
-
-
-def init_parallel_simple(dists_arr):
-    global dists
-    dists = dists_arr
-
-def floyd_parallel_simple(graph_initial, n):
+    n = graph.size  
     # unwrap the 2d graph of edges to a 1d array with i * n + j indexation
+    # and also implicitely create a shallow copy of the graph
     graph_unwrapped = []
-    for row in graph_initial:
+    for row in graph:
         graph_unwrapped += row
-    
     # create a synchronized array that will be available to all processes
     # we don't need locking, because we only use this distance array
     # for 3rd loop of Floyd's algorithm, which is independent
     # of other other iterations
-    graph_unwrapped_synced = mp.Array('i', graph_unwrapped, lock=False)
-
-    for k in range(n):
-        # the pool that executes the 3rd loop of Floyd's algorithm
-        # here we initialize the dist array
-        # so that it is available inside the worker functions
-        pool = mp.Pool(initializer=init_parallel_simple, initargs=(graph_unwrapped_synced,))
-        # for each i (2nd loop of Floyd's algorithm)
-        # we execute all loops for j (3rd loop) in parallel
-        pool.map(worker_simple, [(n, k, i) for i in range(n)])
-        # finalize parallelization and sync results
-        pool.close()
-        pool.join()
-        
+    graph_unwrapped_synced = SynchronizedArray('i', graph_unwrapped, lock=False)
+    # the pool that executes the 3rd loop of Floyd's algorithm
+    # here we initialize the dist array
+    # so that it is available inside the worker functions
+    with ProcessPoolExecutor(max_workers=4, initializer=init_parallel, initargs=(graph_unwrapped_synced,)) as executor:
+        for k in range(n):
+            # for each i (2nd loop of Floyd's algorithm)
+            # we execute all loops for j (3rd loop) in parallel
+            print(k)
+            for i in range(n):
+                executor.submit(worker, n, k, i)
+    # notice: we don't need to wait for all processes to finish in each iteration of the
+    # k loop, because the i loop processes are independent
+    # -------------------------------------------------
     # bring back the distances representation to 2d-array
     result = []
     for i in range(n):
@@ -74,14 +42,20 @@ def floyd_parallel_simple(graph_initial, n):
         for j in range(n):
             row.append(graph_unwrapped_synced[i * n + j])
         result.append(row)
-
     return result
 
 
-def worker_simple(args):
-    n, k, i = args
-    # important: use the synced array between processes
+def init_parallel(dists_arr):
+    # making the shared memory variable global
+    # makes sure that it is accessible from all process workers
+    # any other configurations do not allow concurrent updates
     global dists
+    dists = dists_arr
+
+
+def worker(n, k, i):
+    # important: use the synced array between processes
+    # ---------------------------------------------
     # 3rd loop of Floyd's algorithm
     for j in range(n):
         if (dists[i * n + k] >= 0 and dists[k * n + j] >= 0):
